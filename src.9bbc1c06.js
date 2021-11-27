103249a00a7c3bacf391be6e2e4ebe89000a08da
riko1212 <46224717+riko1212@users.noreply.github.com>
2021-11-27 14:01:52 +0000

    Deploying to gh-pages from @ riko1212/fe-43-project@a6965fa5b0c75cec0307c3c4534ed26e84dc32d5 🚀

--- src.9bbc1c06.js
+++ src.9bbc1c06.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/fe-43-project/src.5997820f.js.map
+//# sourceMappingURL=/fe-43-project/src.9bbc1c06.js.map
